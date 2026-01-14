4fe99e4ca4b9a6c466aa3fb392fcca0ab1e188ee
SurajYadav8 <raosahab1e@gmail.com>
2026-01-14 18:41:23 +0000

    deploy script

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -3,10 +3,10 @@
 pragma solidity ^0.8.18;
 
 import {UUPSUpgradeable} from "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
-import {Initializable} from "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+// import {Initializable} from "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 
-contract BoxV2 is UUPSUpgradeable, OwnableUpgradeable, Initializable {
+contract BoxV2 is UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
 
     constructor() {
@@ -14,12 +14,13 @@ contract BoxV2 is UUPSUpgradeable, OwnableUpgradeable, Initializable {
     }
 
     function initialize() public initializer {
-        __Ownable_init();
-        __UUPSUpgradeable_init();
+        __Ownable_init(msg.sender);
+        // __UUPSUpgradeable_init();
     }
 
-    function setNumber(uint256 _number) external {}
-
+    function setNumber(uint256 _number) external {
+        number = _number;
+    }
     function getNumber() external view returns (uint256) {
         return number;
     }
