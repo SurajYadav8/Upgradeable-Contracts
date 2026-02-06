494cd64fdfd5dd297db7db8ac4c705cad25119c9
SurajYadav8 <raosahab1e@gmail.com>
2026-02-06 10:58:18 +0000

    updates

--- script/DeployBox.s.sol
+++ script/DeployBox.s.sol
@@ -7,7 +7,7 @@ import {BoxV1} from "../src/BoxV1.sol";
 import {ERC1967Proxy} from "../lib/openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 
-contract UpgradeBox is Script {
+contract DeployBox is Script {
 
     function run() external returns (address) {
         address proxy = deployBox();
